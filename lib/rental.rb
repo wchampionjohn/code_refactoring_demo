82c20ef99f48c4680e8a571c330c9877e306d9bc
Eddie Kao <eddie@digik.com.tw>
2014-11-27 20:04:47 +0800

    move charge method to Movie class

--- lib/rental.rb
+++ lib/rental.rb
@@ -6,28 +6,12 @@ class Rental
   end
 
   def charge
-    this_amount = 0
-    # determine amounts for each line
-    case movie.price_code
-    when Movie::REGULAR
-      this_amount += 2
-      this_amount += (days_rented - 2) * 1.5 if days_rented > 2
-    when Movie::NEW_RELEASE
-      this_amount += days_rented * 3
-    when Movie::CHILDRENS
-      this_amount += 1.5
-      this_amount += (days_rented - 3) * 1.5 if days_rented > 3
-    end
-    this_amount
+    @movie.charge(@days_rented)
   end
 
   def frequent_renter_points
-    frequent_renter_points = 1
     # 如果是新片而且租超過 1 天，另外加 1 點
-    if movie.price_code == Movie::NEW_RELEASE && days_rented > 1
-      frequent_renter_points += 1
-    end
-    frequent_renter_points
+    (movie.price_code == Movie::NEW_RELEASE && days_rented > 1) ? 2 : 1
   end
 
 end
