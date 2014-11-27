149221cfcef58038bd3f4f7798e77afcdf0bd417
Eddie Kao <eddie@digik.com.tw>
2014-11-27 19:59:03 +0800

    move frequent_renter_points to total_frequent_renter_points

--- lib/customer.rb
+++ lib/customer.rb
@@ -11,26 +11,25 @@ class Customer
   end
 
   def statement
-    total_amount = 0            # 總消費金額
-    frequent_renter_points = 0  # 常客積點
     result = "Rental Record for #{@name}\n"
 
     @rentals.each do |rental|
-      frequent_renter_points += rental.frequent_renter_points
-
       # 顯示此筆租借資料
       result += "\t" + rental.movie.title + "\t" + rental.charge.to_s + "\n"
-      total_amount += rental.charge
     end
 
     # 結尾列印
     result += "Amount owed is #{total_charge}\n"
-    result += "You earned #{frequent_renter_points} frequent renter points"
+    result += "You earned #{total_frequent_renter_points} frequent renter points"
     result
   end
 
   private
   def total_charge
     @rentals.inject(0) { |sum, rental| sum + rental.charge }
+  end
+
+  def total_frequent_renter_points
+    @rentals.inject(0) { |sum, rental| sum + rental.frequent_renter_points }
   end
 end
