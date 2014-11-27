b1ffa7d71e15ef8dc3c84b71d432da46ef243788
Eddie Kao <eddie@digik.com.tw>
2014-11-27 20:19:45 +0800

    replace case..when with Classes

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,29 +1,50 @@
+class RegularPrice
+  def charge(days_rented)
+    result = 2
+    result += (days_rented - 2) * 1.5 if days_rented > 2
+  end
+end
+
+class NewReleasePrice
+  def charge(days_rented)
+    result = 0
+    result += days_rented * 3
+  end
+end
+
+class ChildrensPrice
+  def charge(days_rented)
+    result = 1.5
+    result += (days_rented - 3) * 1.5 if days_rented > 3
+  end
+end
+
+# --------------------------------
+
 class Movie
   REGULAR     = 0     # 普通片
   NEW_RELEASE = 1     # 新片
   CHILDRENS   = 2     # 兒童片
 
   attr_reader :title
-  attr_accessor :price_code
+  attr_reader :price_code
 
   def initialize(title, price_code)
-    @title, @price_code = title, price_code
+    @title = title
+    self.price_code = price_code
+  end
+
+  def price_code=(new_value)
+    @price_code = new_value
+    @price = case price_code
+             when REGULAR; RegularPrice.new
+             when NEW_RELEASE; NewReleasePrice.new
+             when CHILDRENS; ChildrensPrice.new
+             end
   end
 
   def charge(days_rented)
-    result = 0
-    # determine amounts for each line
-    case price_code
-    when Movie::REGULAR
-      result += 2
-      result += (days_rented - 2) * 1.5 if days_rented > 2
-    when Movie::NEW_RELEASE
-      result += days_rented * 3
-    when Movie::CHILDRENS
-      result += 1.5
-      result += (days_rented - 3) * 1.5 if days_rented > 3
-    end
-    result
+    @price.charge(days_rented)
   end
 
   def frequent_renter_points(days_rented)
